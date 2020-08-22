d41264c660329050ce8512f93259dfb0749f9e86
metamodern <shabanovbunya@gmail.com>
2020-08-22 21:08:44 +0300

    returned lazy loading
    also add loader circle

---
+++ client/src/components/PageLoading/PageLoading.style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles((theme) => {
+  return {
+    root: {
+      height: "100%",
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  };
+});
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import PageLoading from "./components/PageLoading/PageLoading";
 
-import TimerPage from "./pages/TimerPage/TimerPage";
-import TasksPage from "./pages/TasksPage/TasksPage";
-import SettingsPage from "./pages/SettingsPage/SettingsPage";
-import LoginPage from "./pages/AuthPages/LoginPage";
-import RegisterPage from "./pages/AuthPages/RegisterPage";
+// import TimerPage from "./pages/TimerPage/TimerPage";
+// import TasksPage from "./pages/TasksPage/TasksPage";
+// import SettingsPage from "./pages/SettingsPage/SettingsPage";
+// import LoginPage from "./pages/AuthPages/LoginPage";
+// import RegisterPage from "./pages/AuthPages/RegisterPage";
 
-// const TimerPage = lazy(() => import("./pages/TimerPage/TimerPage"));
-// const TasksPage = lazy(() => import("./pages/TasksPage/TasksPage"));
-// const SettingsPage = lazy(() => import("./pages/SettingsPage/SettingsPage"));
-// const LoginPage = lazy(() => import("./pages/AuthPages/LoginPage"));
-// const RegisterPage = lazy(() => import("./pages/AuthPages/RegisterPage"));
+const TimerPage = lazy(() => import("./pages/TimerPage/TimerPage"));
+const TasksPage = lazy(() => import("./pages/TasksPage/TasksPage"));
+const SettingsPage = lazy(() => import("./pages/SettingsPage/SettingsPage"));
+const LoginPage = lazy(() => import("./pages/AuthPages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/AuthPages/RegisterPage"));
 export default function getRoutes(isAuth) {
   const authRoutes = [
     <Route path="/tasks" key="/tasks" component={TasksPage} />,
@@ -22,10 +23,12 @@ export default function getRoutes(isAuth) {
     <Route path="/register" key="/register" component={RegisterPage} />,
   ];
   return (
-    <Switch>
-      <Route path="/" key="/" exact component={TimerPage} />
-      {isAuth ? authRoutes : nonAuthRoutes}
-      <Redirect to="/" />
-    </Switch>
+    <Suspense fallback={<PageLoading />}>
+      <Switch>
+        <Route path="/" key="/" exact component={TimerPage} />
+        {isAuth ? authRoutes : nonAuthRoutes}
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
   );
 }
